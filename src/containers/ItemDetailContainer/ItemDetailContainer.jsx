1d703d004186462cfc01b7ca12781b61cf6bc018
DianaMamani <dianamamanillan@gmail.com>
2023-05-29 22:49:26 -0300

    added firebase config

--- src/containers/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/containers/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,18 +1,21 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getProductById } from "../../asyncMock";
 import { ItemDetail } from "../../components/ItemDetail/ItemDetail";
+import { db } from "../../firebase/config";
+import { getDoc, doc } from "firebase/firestore/lite";
 
 export const ItemDetailContainer = () =>{
     const[itemInfo, setItemInfo] = useState(null);
     const itemId = useParams().id;
     
     useEffect(() => {
-        getProductById(itemId)
-            .then((response) =>{
-                setItemInfo(response);
+        const item = doc(db, "products", itemId)
+
+        getDoc(item)
+            .then(doc => {
+                setItemInfo({ ...doc.data(), id: doc.id })
             })
-            .catch((err => console.log(err)));
+            .catch((err => console.log(err)))
     },[itemId]);
 
     return (
--- src/containers/ItemListContainer/ItemListContainer.jsx
+++ src/containers/ItemListContainer/ItemListContainer.jsx
@@ -1,20 +1,33 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import { getProducts } from "../../asyncMock";
 import { ItemList } from "../../components/ItemList/ItemList";
 import { useParams } from "react-router-dom";
+import { collection, getDocs, query, where } from "firebase/firestore/lite";
+import { db } from "../../firebase/config";
 
 export const ItemListContainer = ({greeting}) => {
     const [products, setProducts] = useState([]);
     const categoryId = useParams().id;
 
     useEffect(() => {
-        getProducts()
-            .then(response => {
-                if(categoryId) setProducts(response.filter((x) => x.category === categoryId));
-                else setProducts(response);
+        const products = collection(db, "products");
+
+        const customQuery = categoryId
+            ? query(products, where("category", "==", categoryId))
+            : products;
+        
+        getDocs(customQuery)
+            .then((response) => {
+                setProducts(
+                    response.docs.map((doc) => {
+                        return {
+                            ...doc.data(),
+                            id: doc.id,
+                        };
+                    })
+                );
             })
-            .catch(err => { console.log("something is bad ", err); })
+            .catch(err => { console.log("something is bad ", err); });  
     }, [categoryId])
 
     return (
